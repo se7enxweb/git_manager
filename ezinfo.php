f8b6a50c5304b9854ca572323de86ffa2cdda1fb
7x <info@se7enx.com>
2024-10-18 22:42:37 +0000

    Added: Added extension documentation, license, composer support and extension.xml and sponsorship info

--- ezinfo.php
+++ ezinfo.php
@@ -10,12 +10,12 @@ class git_managerInfo
 {
 	public static function info() {
 		return array(
-			'Name'      => '<a href="http://projects.ez.no/git_manager">Git Manager</a>',
+			'Name'      => '<a href="https://github.com/se7enxweb/git_manager">Git Manager</a>',
 			'Version'   => '1.0.1',
-			'Author'    => 'Serhey Dolgushev',
-			'Copyright' => 'Copyright &copy; ' . date( 'Y' ) . ' <a href="http://ua.linkedin.com/in/serheydolgushev" target="blank">Serhey Dolgushev</a>',
+			'Author'    => '7x and Serhey Dolgushev',
+			'Copyright' => 'Copyright &copy; ' . date( 'Y' ) . ' <a href="http://ua.linkedin.com/in/serheydolgushev" target="blank">Serhey Dolgushev</a> and 1999 - 2024 7x',
                         'License' => "GNU General Public License",
-                        'info_url'  => 'https://github.com/brookinsconsulting/git_manager'
+                        'info_url'  => 'https://github.com/se7enxweb/git_manager'
                         
 		);
 	}
