5022a7c78e11ff0e8c3a85fc05d6197633dd2436
7x <info@se7enx.com>
2026-06-22 00:27:04 -0700

    Updated: Updated ezinfo.php and extension.xml version file. Doc.

--- ezinfo.php
+++ ezinfo.php
@@ -11,9 +11,9 @@ class git_managerInfo
 	public static function info() {
 		return array(
 			'Name'      => '<a href="https://github.com/se7enxweb/git_manager">Git Manager</a>',
-			'Version'   => '1.0.1',
+			'Version'   => '2.0.1',
 			'Author'    => '7x and Serhey Dolgushev',
-			'Copyright' => 'Copyright &copy; ' . date( 'Y' ) . ' <a href="http://ua.linkedin.com/in/serheydolgushev" target="blank">Serhey Dolgushev</a> and 1999 - 2024 7x',
+			'Copyright' => 'Copyright &copy; ' . date( 'Y' ) . ' <a href="http://ua.linkedin.com/in/serheydolgushev" target="blank">Serhey Dolgushev</a> and 1998 - ' . date( 'Y' ) . ' 7x',
                         'License' => "GNU General Public License",
                         'info_url'  => 'https://github.com/se7enxweb/git_manager'
                         
